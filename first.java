94e30bdd081c53f6360edc42da2493548c1cc18d
tharunsoganur <tharun.20mis7030@gmail.com>
2021-03-06 11:52:48 +0530

    mirror changes to first.java

--- first.java
+++ first.java
@@ -4,11 +4,13 @@ public class AddTwoIntegers {
     public static void main(String[] args) {
         
         int first = 10;
-        int second = 20;
+        int second = 30;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
 
         System.out.println("The sum is: " + sum);
+	System.out.println("The sum is: " + sum);
+
     }
 }
